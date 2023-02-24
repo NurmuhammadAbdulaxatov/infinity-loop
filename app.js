e21b8aa7f7710b7572715a87e2f27ff9f8c8b996
NurmuhammadAbdulaxatov <abdulaxatov3@gmail.com>
2023-02-24 21:30:45 +0500

    success

--- app.js
+++ app.js
@@ -1,4 +1,34 @@
-for (let i = 0; i < Infinity; i++) {
-  let loop;
-  loop++;
+// for (let i = 0; i < Infinity; i++) {
+//   let loop;
+//   loop++;
+// }
+
+// for (;;) {}
+
+// while (true) {
+//   let a;
+//   a++;
+// }
+
+// let a = 0;
+// let b = 0;
+
+// if (a === b) {
+//   a + b;
+// } else {
+//   console.log("uuu");
+// }
+
+// let a;
+
+// while (a === Infinity) {
+//   a++;
+// }
+
+for (let i = 1; true; i++) {
+  if (i % 2 == 0) {
+    console.log("Loop");
+  } else {
+    console.log("Sabr");
+  }
 }
